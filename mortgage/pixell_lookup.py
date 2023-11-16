da95a6688280183b13b33b6f108831c0b4abdd46
DongokYang <dyang2@academic.rrc.ca>
2023-11-16 14:47:59 -0600

    MortgageRate and PaymentFrequency classes have added

--- mortgage/pixell_lookup.py
+++ mortgage/pixell_lookup.py
@@ -9,8 +9,20 @@ with mortgages to ensure only valid rates, frequencies and amortization
 periods are used.
 """
 
-
 from enum import Enum
 
 VALID_AMORTIZATION = [5, 10, 15, 20, 25, 30] 
+
+class MortgageRate(Enum):
+    FIXED_5 = 0.0500
+    FIXED_3 = 0.0579
+    FIXED_1 = 0.0589
+    VARIABLE_5 = 0.0650
+    VARIABLE_3 = 0.0660
+    VARIABLE_1 = 0.0679
+
+class PaymentFrequency(Enum):
+    MONTHLY = 12
+    BI_WEEKLY = 26
+    WEEKLY = 52
 
